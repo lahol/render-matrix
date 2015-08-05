687a90b492ffd851c7d559e79405af5b569132c1
Holger Langenau <holger.langenau@mathematik.tu-chemnitz.de>
2015-08-05 11:11:57 +0200

    Preparations for height -> color.

---
+++ graphics.c
@@ -0,0 +1,38 @@
+
+/* get rgb values for (101->001->011->010->110->100)
+ * [magenta -> blue -> cyan -> green -> yellow -> red]
+ * @in: hue in [0,1]
+ * @out: rgb [0,1]^3
+ */
+void color_gradient_rgb(double hue, double *rgb)
+{
+#define _N_COLORS 6
+    static double basic_table[_N_COLORS+1][3] = {
+        /* { 1.0, 1.0, 1.0 }, */
+        { 1.0, 0.0, 1.0 },
+        { 0.0, 0.0, 1.0 },
+        { 0.0, 1.0, 1.0 },
+        { 0.0, 1.0, 0.0 },
+        { 1.0, 1.0, 0.0 },
+        { 1.0, 0.0, 0.0 },
+        { 0.0, 0,0, 0.0 } /* currently only for index, although this should be caught in the next if clause */
+    };
+
+    if (hue >= 1.0) {
+        rgb[0] = 1.0; rgb[1] = 0.0; rgb[2] = 0.0;
+        return;
+    }
+    if (hue <= 0.0) {
+        rgb[0] = 1.0; rgb[1] = 0.0; rgb[2] = 1.0;
+        return;
+    }
+
+    int index = (int)((_N_COLORS - 1) * hue);       /* floor */
+    double lambda = ((_N_COLORS - 1) * hue - index); /* frac */
+
+    rgb[0] = (1.0 - lambda) * basic_table[index][0] + lambda * basic_table[index + 1][0];
+    rgb[1] = (1.0 - lambda) * basic_table[index][1] + lambda * basic_table[index + 1][1];
+    rgb[2] = (1.0 - lambda) * basic_table[index][2] + lambda * basic_table[index + 1][2];
+
+#undef _N_COLORS
+}
